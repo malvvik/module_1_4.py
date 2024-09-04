93ace50580ada458e5a78f0c7026c58d37a73209
malvvik <malvvik@gmail.com>
2024-09-04 12:39:47 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = input("Сколько стоит дирижабль?: ")
+print("Длина текста:", len (my_string))
+print("Текст в верхнем регистре:", my_string.upper())
+print("Текст в нижнем регистре:", my_string.lower())
+print("Текст без пробелов:", my_string.replace(' ', ''))
+print("Первый символ строки: ", my_string[0])
+print("Последний символ строки:", my_string[-1])
+
+
+
+
+
+
+
